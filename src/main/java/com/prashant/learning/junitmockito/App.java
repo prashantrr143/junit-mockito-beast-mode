102de6b232a79086b8c198f35ad1163260eaffe0
Prashant Kumar Singh <prashant.sirt153@gmail.com>
2019-11-12 01:37:58 +0530

    Init Commit

---
+++ src/main/java/com/prashant/learning/junitmockito/App.java
@@ -0,0 +1,13 @@
+package com.prashant.learning.junitmockito;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
